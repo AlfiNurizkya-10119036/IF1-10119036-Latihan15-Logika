b5ae77b29b7183aeb67a4c53f9002f53177d1a6c
ALFI22-01 <72850530+ALFI22-01@users.noreply.github.com>
2020-10-15 14:12:02 +0700

    Project 15

---
+++ src/PBO10119036Latihan15Logika.java
@@ -0,0 +1,23 @@
+
+/**
+ *
+ * @author Alfi Nurizkya
+ * NAMA     : ALFI NURIZKYA
+ * KELAS    : PBO
+ * NIM      : 10119036
+ * DESKRIPSI PROGRAM : PERINTAH MEMASUKKAN DATA BIT
+ */
+public class PBO10119036Latihan15Logika {
+
+    
+    public static void main(String[] args) {
+     
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
